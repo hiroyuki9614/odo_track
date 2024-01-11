1992621fd9d4f1f171e55af7aec8efbe4df48584
hiroyuki9614 <mmnnoov@gmail.com>
2024-01-11 13:24:34 +0900

    delete frequent_destination_table

--- db/migrate/20231118031334_create_frequent_destinations.rb
+++ db/migrate/20231118031334_create_frequent_destinations.rb
@@ -1,12 +1,12 @@
-# class CreateFrequentDestinations < ActiveRecord::Migration[7.1]
-#   def change
-#     create_table :frequent_destinations do |t|
-#       t.string :destination_name, null: false, comment: '目的地'
-#       t.text :destination_note, null: false, comment: '目的地についてのメモ'
+class CreateFrequentDestinations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :frequent_destinations do |t|
+      t.string :destination_name, null: false, comment: '目的地'
+      t.text :destination_note, null: false, comment: '目的地についてのメモ'
 
-#       t.references :daily_log, foreign_key: { on_update: :restrict, on_delete: :restrict }, comment: '運転日報参照'
-#       t.references :user_id, foreign_key: { on_update: :restrict, on_delete: :restrict }, comment: 'ユーザー参照'
-#       t.timestamps
-#     end
-#   end
-# end
+      t.references :daily_log, foreign_key: { on_update: :restrict, on_delete: :restrict }, comment: '運転日報参照'
+      # t.references :user_id, foreign_key: { on_update: :restrict, on_delete: :restrict }, comment: 'ユーザー参照'
+      t.timestamps
+    end
+  end
+end
