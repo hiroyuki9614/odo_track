a23d06af5b6f2e4e1441525f81a800b84c89b382
hiroyuki9614 <mmnnoov@gmail.com>
2024-01-12 15:02:25 +0900

    change .git

--- db/schema.rb
+++ db/schema.rb
@@ -10,84 +10,85 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_19_094524) do
-  create_table "daily_logs", force: :cascade do |t|
-    t.datetime "departure_datetime", null: false
-    t.datetime "arrival_datetime", null: false
-    t.integer "departure_distance", null: false
-    t.integer "arrival_distance", null: false
-    t.string "departure_location", null: false
-    t.string "arrival_location", null: false
-    t.text "note", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "is_alcohol_check", default: false, null: false
-    t.boolean "is_studless_tire", default: false, null: false
-    t.integer "approval_status", default: 0, null: false
-    t.integer "user_id"
-    t.datetime "discarded_at"
-    t.integer "vehicle_id", null: false
-    t.index ["user_id", "created_at"], name: "index_daily_logs_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_daily_logs_on_user_id"
+ActiveRecord::Schema[7.1].define(version: 20_231_219_094_524) do
+  create_table 'daily_logs', force: :cascade do |t|
+    t.datetime 'departure_datetime', null: false
+    t.datetime 'arrival_datetime', null: false
+    t.integer 'departure_distance', null: false
+    t.integer 'arrival_distance', null: false
+    t.string 'departure_location', null: false
+    t.string 'arrival_location', null: false
+    t.text 'note', default: '', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.boolean 'is_alcohol_check', default: false, null: false
+    t.boolean 'is_studless_tire', default: false, null: false
+    t.integer 'approval_status', default: 0, null: false
+    t.integer 'user_id'
+    t.datetime 'discarded_at'
+    t.integer 'vehicle_id', null: false
+    t.index %w[user_id created_at], name: 'index_daily_logs_on_user_id_and_created_at'
+    t.index ['user_id'], name: 'index_daily_logs_on_user_id'
   end
 
-  create_table "favorite_vehicles", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "vehicle_id", null: false
-    t.text "favorite_vehicle_note", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_favorite_vehicles_on_user_id"
-    t.index ["vehicle_id"], name: "index_favorite_vehicles_on_vehicle_id"
+  create_table 'favorite_vehicles', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'vehicle_id', null: false
+    t.text 'favorite_vehicle_note', default: '', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_favorite_vehicles_on_user_id'
+    t.index ['vehicle_id'], name: 'index_favorite_vehicles_on_vehicle_id'
   end
 
-  create_table "frequent_destinations", force: :cascade do |t|
-    t.string "destination_name", null: false
-    t.text "destination_note", null: false
-    t.integer "daily_log_id"
-    t.integer "user_id_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["daily_log_id"], name: "index_frequent_destinations_on_daily_log_id"
-    t.index ["user_id_id"], name: "index_frequent_destinations_on_user_id_id"
+  create_table 'frequent_destinations', force: :cascade do |t|
+    t.string 'destination_name', null: false
+    t.text 'destination_note', null: false
+    t.integer 'daily_log_id'
+    # t.integer "user_id_id"
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['daily_log_id'], name: 'index_frequent_destinations_on_daily_log_id'
+    # t.index ["user_id_id"], name: "index_frequent_destinations_on_user_id_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "user_name", null: false
-    t.string "email", default: "", null: false
-    t.string "telephone", null: false
-    t.boolean "admin", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "discarded_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["discarded_at"], name: "index_users_on_discarded_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'user_name', null: false
+    t.string 'email', default: '', null: false
+    t.string 'telephone', null: false
+    t.boolean 'admin', default: false, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.datetime 'discarded_at'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['discarded_at'], name: 'index_users_on_discarded_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "vehicles", force: :cascade do |t|
-    t.string "vehicle_name", null: false
-    t.string "number", null: false
-    t.string "manufacture", null: false
-    t.integer "current_drive_distance", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "discarded_at"
-    t.index ["vehicle_name", "number", "manufacture"], name: "index_vehicles_on_vehicle_name_and_number_and_manufacture", unique: true
+  create_table 'vehicles', force: :cascade do |t|
+    t.string 'vehicle_name', null: false
+    t.string 'number', null: false
+    t.string 'manufacture', null: false
+    t.integer 'current_drive_distance', default: 0, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.datetime 'discarded_at'
+    t.index %w[vehicle_name number manufacture],
+            name: 'index_vehicles_on_vehicle_name_and_number_and_manufacture', unique: true
   end
 
-  add_foreign_key "daily_logs", "users", on_update: :restrict, on_delete: :restrict
-  add_foreign_key "favorite_vehicles", "users", on_delete: :restrict
-  add_foreign_key "favorite_vehicles", "vehicles", on_delete: :restrict
-  add_foreign_key "frequent_destinations", "daily_logs", on_update: :restrict, on_delete: :restrict
-  add_foreign_key "frequent_destinations", "user_ids", on_update: :restrict, on_delete: :restrict
+  add_foreign_key 'daily_logs', 'users', on_update: :restrict, on_delete: :restrict
+  add_foreign_key 'favorite_vehicles', 'users', on_delete: :restrict
+  add_foreign_key 'favorite_vehicles', 'vehicles', on_delete: :restrict
+  add_foreign_key 'frequent_destinations', 'daily_logs', on_update: :restrict, on_delete: :restrict
+  # add_foreign_key "frequent_destinations", "user_ids", on_update: :restrict, on_delete: :restrict
 end
