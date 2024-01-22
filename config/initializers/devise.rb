35ef29ed1583f91c3863e2fc83669b74e0ebdbb2
hiroyuki9614 <mmnnoov@gmail.com>
2024-01-22 22:57:27 +0900

    mail setting

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'pikupin9441@gmail.com'
+  config.mailer_sender = 'no_reply@sandboxb71c5c51498046ceb4464e8db6d4aa61.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
