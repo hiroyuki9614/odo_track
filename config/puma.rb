1b7893fcfd67765025099b025db04c19cae3bccb
hiroyuki9614 <mmnnoov@gmail.com>
2024-02-03 10:14:01 +0900

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,14 @@
-bind 'tcp://0.0.0.0:3000'
+# bind 'tcp://0.0.0.0:3000'
 
 # Pumaの設定ファイル
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+port        ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { ENV['RACK_ENV'] || 'production' }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 preload_app!
 plugin :tmp_restart
-app_root = File.expand_path("../..", __FILE__)
+app_root = File.expand_path('..', __dir__)
 bind "unix:#{app_root}/tmp/sockets/puma.sock"
