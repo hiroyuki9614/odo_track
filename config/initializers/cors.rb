d4c0f0bbd11feeaab57aa1bec0f85971192a70a7
hiroyuki9614 <mmnnoov@gmail.com>
2024-02-04 21:14:37 +0900

    change coros setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://0.0.0.0:3000' # 許可するオリジン（ドメイン）を指定
-    origins 'https://h9614.link/'
+    # origins 'https://h9614.link/'
+    origins '*'
     resource '*', # すべてのリソースに適用
              headers: :any, # すべてのヘッダーを許可
              methods: :any, # 許可するHTTPメソッド
